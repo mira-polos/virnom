602eb2390128263db4024496428d14d038a2a1aa
Mira polos <66188045+mira-polos@users.noreply.github.com>
2020-06-03 19:47:15 +0700

    Add files via upload

---
+++ virnom.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+#tools mira tersakiti x-target indonesia
+#prank kirim virus telpon
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+
+echo $green "┈┈┈╲┈┈┈┈╱"
+echo $green "┈┈┈╱  ▔▔╲"
+echo $green "┈┈┃┈▇┈┈▇┈┃" ""    $cyan ">>> TOOL MIRA TERSAKITI <<<"
+echo $green "╭╮┣━━━━━━┫╭╮"
+echo $green "┃┃┃┈┈┈┈┈┈┃┃┃"  $red ─────╔╗──────────────
+echo $green "╰╯┃┈┈┈┈┈┈┃╰╯"    $red ╔═╦═╗╠╣╔╦╗╔═╦╗╔═╗╔══╗
+echo $green "┈┈╰┓┏━━┓┏╯ " ""    $red ╚╗║╔╝║║║╔╝║║║║║╬║║║║║
+echo $green "┈┈┈╰╯┈┈╰╯ " ""  ""  $red "─╚═╝─╚╝╚╝─╚╩═╝╚═╝╚╩╩╝"
+echo $green
+echo -n "nomor korban: ";read lp
+sleep 2
+clear
+while true
+do
+echo "SUKSES MENGIRIM VIRUS BERBAHAYA KE NOMOR $lp"
+sleep 1
+done
+
+
+
+
